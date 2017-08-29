3ca0496af078d6f272072213de89d253d263cf44
PLH <admin@AR-113-03>
2017-08-29 14:29:49 +0200

    added method

---
+++ Module01_CS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Module01_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Her er min første test");
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
