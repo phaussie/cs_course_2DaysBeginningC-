e8fba3bf337cff07e9b3906566cbc66dd015442c
PLH <admin@AR-113-03>
2017-08-30 15:43:54 +0200

    new code added

--- Module05_Methods/Program.cs
+++ Module05_Methods/Program.cs
@@ -10,12 +10,44 @@ namespace Module05_Methods
     {
         static void Main(string[] args)
         {
+            Test1();
+            //Test2(98);
+            Test1(12);
+
+            int res = Test3(1, 4);
+
+            Console.WriteLine(res);
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 System.Console.Write("Press any key to continue . . . ");
                 System.Console.ReadKey();
             }
+        }
+
+        static void Test1()
+        {
+            int i = 1;
+
+            Test2(i);
+
+        }
+
+        static void Test2(int y)
+        {
+            int i = 1;
+
+        }
+
+        static void Test1(int y)
+        {
+            int i = 1;
+
+        }
+
+        static int Test3(int a, int b)
+        {
+            return a + b;
         }
     }
 }
